704ab336464430a22f6244733b536bf0db22b303
Handsome2Hu <370981805@qq.com>
2017-09-11 21:47:41 +0800

    正在学着撸的爬虫

---
+++ url_manager.py
@@ -0,0 +1,16 @@
+class UrlManager(object):
+    
+    def add_new_url(self):
+        pass
+   
+   
+    def add_new_urls(self):
+        pass
+    
+    
+    def has_new_url(self):
+        pass
+    
+    
+    def get_new_url(self):
+        pass
