53f935e662bff6cd41fca7630c01ab4e7ac17c9d
Handsome2Hu <370981805@qq.com>
2017-09-12 09:59:46 +0800

    Revert "Revert "做了一部分""
    This reverts commit b9f73d768def90527322703c0f48a3ddf7619ac5.

--- html_downloader.py
+++ html_downloader.py
@@ -1,3 +1,12 @@
+import urllib.request
+
 class HtmlDownloader(object):
-    def download(self):
-        pass
+    
+    def download(self,url):
+        if url is None:
+            return None
+        
+        response = urllib.request.urlopen(url)
+        if response.getcode() != 200:
+            return None
+        return response.read()
